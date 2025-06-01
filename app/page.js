3d62ea9a3dc30c69ffa42ced8138ceebebf0a743
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-02 03:52:58 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,48 @@
+"use client"
+import Image from "next/image";
+import { useState, useRef, useEffect, useReducer } from "react";
+import { io } from "socket.io-client";
+
+export default function Home() {
+  const textAreaRef = useRef(null)
+  const [value, setValue] = useState("")
+  const [cursor, setCursor] = useState(0)
+  const [user, setUser] = useState({}) 
+  const valueRef = useRef("")
+  const socketRef = useRef(null)
+  console.log(cursor)
+
+  useEffect(() => {
+    const socket = io('https://localhost:3000/')
+    socketRef.current = socket
+    socketRef.current.on('cursor-moved',(obj)=>{
+      if(obj.operation==="add")
+        textAreaRef.current.value = textAreaRef.current.value.slice(0, obj.cursor) + obj.text + textAreaRef.current.value.slice(obj.cursor)
+      else
+        textAreaRef.current.value = textAreaRef.current.value.slice(0, obj.cursor) + textAreaRef.current.value.slice(obj.cursor + 1);
+    })
+  })
+
+  const handleChange = (e) => {
+    textAreaRef.current.value = e.target.value
+    const pos = textAreaRef.current.selectionStart;
+    console.log(pos)
+    if(cursor<pos)
+      socket.emit('cursor-moved', {cursor: pos, text: textAreaRef.current.value[pos], operation: "add"})
+    else
+      socket.emit('cursor-moved', {cursor: pos, text: textAreaRef.current.value[pos], operation: "delete"})
+    setCursor(pos)
+  }
+
+  const handleClick = () => {
+    const pos = textAreaRef.current.selectionStart;
+    console.log(pos)
+    setCursor(pos)
+  }
+  
+  return (
+    <div className="flex">
+      <textarea ref={textAreaRef} onChange={handleChange} onClick={handleClick} className="border border-b-black w-[75vw] h-[85vh] p-12" name="editor"/>
+    </div>
+  );
+}

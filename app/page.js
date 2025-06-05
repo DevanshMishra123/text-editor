5ccf73bfc100c1a67b507cabdb57c5819a8a41b4
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-05 15:01:15 +0530

    changed files

--- app/page.js
+++ app/page.js
@@ -1,40 +1,79 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { useState, useRef, useEffect, useReducer } from "react";
 import { io } from "socket.io-client";
 
 export default function Home() {
-  const textAreaRef = useRef(null)
-  const [cursor, setCursor] = useState(0)
-  const cursorRef = useRef(0)
-  const socketRef = useRef(null)
-  console.log(cursor)
+  const textAreaRef = useRef(null);
+  const [cursor, setCursor] = useState(0);
+  const cursorRef = useRef(0);
+  const socketRef = useRef(null);
+  const prevTextRef = useRef("");
+  console.log(cursor);
 
   useEffect(() => {
-    const socket = io('https://text-editor-backend-nmie.onrender.com/');
+    const socket = io("https://text-editor-backend-nmie.onrender.com/");
     socketRef.current = socket;
 
-    socket.on('connect', () => {
-      console.log('Connected to server:', socket.id);
+    socket.on("connect", () => {
+      console.log("Connected to server:", socket.id);
     });
 
-    socket.on('connect_error', (err) => {
-      console.error('Connection error:', err.message);
+    socket.on("connect_error", (err) => {
+      console.error("Connection error:", err.message);
     });
-    socketRef.current.on('cursor-moved',(obj)=>{
-      if(obj.operation==="add")
-        textAreaRef.current.value = textAreaRef.current.value.slice(0, obj.cursor) + obj.text + textAreaRef.current.value.slice(obj.cursor)
-      else
-        textAreaRef.current.value = textAreaRef.current.value.slice(0, obj.cursor) + textAreaRef.current.value.slice(obj.cursor + 1);
-    })
-
+    socketRef.current.on("cursor-moved", (obj) => {
+      const text = textAreaRef.current.value;
+      if (obj.operation === "add") {
+        textAreaRef.current.value = text.slice(0, obj.cursor) + obj.text + text.slice(obj.cursor);
+      } else if (obj.operation === "delete") {
+        textAreaRef.current.value = text.slice(0, obj.cursor) + text.slice(obj.cursor + obj.text.length);
+      }
+    });
     return () => {
       socket.disconnect();
     };
-  },[])
+  }, []);
 
   const handleChange = (e) => {
-    textAreaRef.current.value = e.target.value
+      const newText = e.target.value;
+      const oldText = prevTextRef.current;
+      const pos = e.target.selectionStart;
+
+      const diffLength = newText.length - oldText.length;
+
+      if (diffLength > 0) {
+        const addedText = newText.slice(pos - diffLength, pos);
+        socketRef.current.emit("cursor-moved", {
+          cursor: pos - diffLength,
+          text: addedText,
+          operation: "add",
+        });
+      } else if (diffLength < 0) {
+        socketRef.current.emit("cursor-moved", {
+          cursor: pos,
+          text: oldText.slice(pos, pos - diffLength),
+          operation: "delete",
+        });
+      }
+      prevTextRef.current = newText;
+      cursorRef.current = pos;
+  };
+
+  const handleClick = () => {
+    const pos = textAreaRef.current.selectionStart;
+    console.log(pos);
+    setCursor(pos);
+  };
+
+  return (
+    <div className="flex">
+      <textarea ref={textAreaRef} onChange={handleChange} onClick={handleClick} className="border border-b-black w-[75vw] h-[85vh] p-12" name="editor"/>
+    </div>
+  );
+}
+/*
+textAreaRef.current.value = e.target.value
     const pos = textAreaRef.current.selectionStart;
     console.log(pos)
     if(cursorRef.current<pos)
@@ -42,17 +81,4 @@ export default function Home() {
     else
       socketRef.current.emit('cursor-moved', {cursor: pos, text: textAreaRef.current.value[cursorRef.current], operation: "delete"})
     cursorRef.current = pos
-  }
-
-  const handleClick = () => {
-    const pos = textAreaRef.current.selectionStart;
-    console.log(pos)
-    setCursor(pos)
-  }
-  
-  return (
-    <div className="flex">
-      <textarea ref={textAreaRef} onChange={handleChange} onClick={handleClick} className="border border-b-black w-[75vw] h-[85vh] p-12" name="editor"/>
-    </div>
-  );
-}
+*/

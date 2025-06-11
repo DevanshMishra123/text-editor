0045e14ca49878cff2583e1175004dba97df997c
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-11 11:14:43 +0530

    changed file

--- app/components/Edit.js
+++ app/components/Edit.js
@@ -33,6 +33,9 @@ export default function Edit() {
     )
   ).current;
 
+  console.log("updated cursor position at anchor position:", editor.selection.anchor.offset)
+  console.log("updated cursor position at focus position:", editor.selection.focus.offset)
+
   const waitForPathAndSelect = (editor, newPath, maxAttempts = 10) => {
     let attempts = 0;
 

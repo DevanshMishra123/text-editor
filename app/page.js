37cdbfc22c2fa16331794332ca731ce66e5eb3e2
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-12 22:08:17 +0530

    changed file

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ const Edit = dynamic(() => import("./components/Edit"), { ssr: false });
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
+    <div className="flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1544894079-e81a9eb1da8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
       <div className="absolute inset-0 bg-white/70 backdrop-blur-md shadow-lg p-8">
         <Edit />
       </div>

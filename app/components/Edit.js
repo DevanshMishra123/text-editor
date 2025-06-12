9ef9d89c8b2d0760ed023e19a4ecc98910c2408a
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-12 20:30:17 +0530

    changed file

--- app/components/Edit.js
+++ app/components/Edit.js
@@ -149,7 +149,7 @@ export default function Edit() {
           const newPath = Path.next(parentPath); 
           console.log("New sibling path:", newPath);
           if (Text.isText(node) && (offset === node.text.length || offset === 0)){
-            const toInsert = offset === 0 ? path : newPath
+            const toInsert = offset === 0 ? parentPath : newPath
             const insertedNode = {
               type: parentNode.type,
               children: [{ text: "" }],

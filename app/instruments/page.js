787d6abe463285c82db1dd1ff4a5b5e8c2606fa8
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-15 04:04:56 +0530

    changed file

--- app/instruments/page.js
+++ app/instruments/page.js
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Instruments() {
   const supabase = await createClient();
+  console.log("supabase is:", supabase)
   const { data: userData } = await supabase.auth.getUser()
 
   if (!userData?.user) return <p>Unauthorized</p>

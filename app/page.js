ff66e961125c766fb5975d4faa083187eb79eb53
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-14 23:31:37 +0530

    changed file

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import Dashboard from "./dashboard/page";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Page() {
+  const supabase = await createClient();
+  const { data: userData } = await supabase.auth.getUser()
+  if (!userData?.user) return <p>Unauthorized</p>
   return (
     <div>
       <Dashboard />

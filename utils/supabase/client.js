b5cccc43e8a4f46cf4d988db139aab37994e8773
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-14 17:41:22 +0530

    changed file

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('Supabase ANON KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY

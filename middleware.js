0cae5751376e55fcb1b1c51fb090817f8c3b53bb
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-16 01:39:38 +0530

    changed files

--- middleware.js
+++ middleware.js
@@ -9,7 +9,7 @@ export async function middleware(req) {
 
     const {
         data: { session },
-    } = await supabase.auth.getUser()
+    } = await supabase.auth.getSession()
 
   
     if (!session && protectedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {

822f064985d016e6b2640fdc71bcd4b98b24fad3
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-15 22:10:47 +0530

    changed file

--- middleware.js
+++ middleware.js
@@ -13,7 +13,7 @@ export async function middleware(req) {
 
   
     if (!session && protectedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {
-        return NextResponse.redirect(new URL('/login', req.url))
+        return NextResponse.redirect(new URL('/', req.url))
     }
 
     return res

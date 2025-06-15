8585fb98b780bed8c8a537b8f335c2d83360fafa
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-15 12:08:08 +0530

    changed file

--- app/instruments/page.js
+++ app/instruments/page.js
@@ -1,13 +1,14 @@
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Instruments() {
-  const supabase = await createClient();
-  console.log("supabase is:", supabase)
-  const { data: userData } = await supabase.auth.getUser()
-
-  if (!userData?.user) return <p>Unauthorized</p>
 
   const { data: instruments } = await supabase.from("instruments").select(); 
 
   return <pre>{JSON.stringify(instruments, null, 2)}</pre>
 }
+/*
+const supabase = await createClient();
+  const { data: userData } = await supabase.auth.getUser()
+
+  if (!userData?.user) return <p>Unauthorized</p>
+*/

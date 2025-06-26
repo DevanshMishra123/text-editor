5887f5a8223598e0b02736580afed0b7fa42d490
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-26 16:23:30 +0530

    changed file

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -54,7 +54,7 @@ export default function Dashboard() {
   return (
     <div className="flex min-h-screen justify-between bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1544894079-e81a9eb1da8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
       <div className="absolute inset-0 bg-white/40 backdrop-blur-sm shadow-lg p-8">
-        <Edit value={inValue} hasLoaded={hasLoaded} />
+        <Edit inValue={inValue} hasLoaded={hasLoaded} />
       </div>
       <button className="absolute top-0 left-0" onClick={signOut}>Sign Out</button>
     </div>

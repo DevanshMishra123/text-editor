b69aefb95c2e967abab1553993d6ac15a451b0f8
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-27 01:50:36 +0530

    changed file

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -81,7 +81,7 @@ export default function Dashboard() {
             </button>
           </div>
           <div>
-            {docs.map((index,element) => <div key={index} className="rounded bg-emerald-400 hover:bg-emerald-500 transition duration-100 text-white m-4 p-4">{element}</div>)}
+            {docs.map((index,element) => <div key={index} className="rounded bg-emerald-400 hover:bg-emerald-500 transition duration-100 text-white mx-4 my-4 p-4">{element}</div>)}
           </div>
         </div>
         <div className="bg-white/40 backdrop-blur-sm shadow-lg p-8">

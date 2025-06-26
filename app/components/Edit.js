978c402c139b53bd1036ce297b40f95abf43594d
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-26 17:42:55 +0530

    changed file

--- app/components/Edit.js
+++ app/components/Edit.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, { useEffect, useRef, useState, useCallback, useMemo } from "react";
 import { createEditor, Node, Text, Operation, Transforms, Editor, Path } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import { withHistory } from "slate-history";

9354f4a50aba34b2664975b98ac6841dadddfeac
DevanshMishra123 <mishra24devansh@gmail.com>
2025-06-26 15:04:24 +0530

    changed file

--- app/components/Edit.js
+++ app/components/Edit.js
@@ -8,13 +8,9 @@ import { io } from "socket.io-client";
 const initialValue = [
   {
     type: "heading",
-    children: [{ text: "This is a heading" }],
-  },
-  {
-    type: "paragraph",
-    children: [{ text: "And this is a paragraph." }],
-  },
-];
+    children: [{text: ""}]
+  }
+]
 
 export default function Edit({inValue, hasLoaded}) {
   const socketRef = useRef(null);
@@ -416,7 +412,7 @@ export default function Edit({inValue, hasLoaded}) {
 
   return (
     <div className="dark:bg-gray-900 dark:text-white w-[80vw] max-w-4xl h-[80vh] mx-auto mt-10 border border-gray-300 shadow-lg rounded-xl bg-white overflow-hidden">
-      <Slate editor={editor} initialValue={inValue} onChange={setValue}>
+      <Slate editor={editor} initialValue={inValue ? inValue : initialValue} onChange={setValue}>
         <Editable className="w-full h-full p-6 text-base leading-relaxed focus:outline-none focus:ring-0 overflow-auto prose prose-sm sm:prose lg:prose-lg dark:prose-invert scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100" renderElement={renderElement} renderLeaf={renderLeaf} decorate={decorate} placeholder="Start typing..." />
       </Slate>
     </div>
